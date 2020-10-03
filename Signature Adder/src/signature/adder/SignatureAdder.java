0925048cc2bc4bd46face9439fde1c9d7ea0016b
IsaacFigNewton <48895941+IsaacFigNewton@users.noreply.github.com>
2020-10-02 21:25:59 -0700

    Added/modified some foundational code and folders
    The pictures are just there so I don't have to look too hard for them next time I work on this

--- Signature Adder/src/signature/adder/SignatureAdder.java
+++ Signature Adder/src/signature/adder/SignatureAdder.java
@@ -35,87 +35,62 @@ import java.util.*;
 public class SignatureAdder{
    
    public static APImage theImage = new APImage("placeholder.png");
-   
+   public static int photoIndex = 0;
+   public static int numPhotos = 1;
    //                                                                           Main Class \/
    public static void main(String[]args) throws IOException{
         Scanner reader = new Scanner(System.in);
-        
 //        //debug
 //        System.out.println("1");
+        processImage();
+        while (numPhotos - 1 > 0 && photoIndex < numPhotos)
+            processImage();
         
 
    }
    
    
     
-   public static void getImage () throws IOException {
+   public static void processImage () throws IOException {
         try {
 //            //debug
 //            System.out.println("3");
             
             //instantiate variables
-            int categoryIndex;
-            String category;
-            File categoryFolder;
-            File fileFolder;
+            
+            File inputFolder;
 
-            String [] categoriesInFolder;
-            String [] filesInCategory;
+            String [] photos;
 
             ArrayList<String> files = new ArrayList<String> ();
-            int fileIndex = 0;
             String fileName = "";
-            String pathway = "";
 
-//           //debug
-//           System.out.println("3.1");
-            
-           //get a random category
-           categoryIndex = (int)(Math.random()*257);
-           
-//           //debug
-//           System.out.println("3.2");
-           
-           //create a list of potential categories
-           categoryFolder = new File("ObjectCategories");
-           categoriesInFolder = categoryFolder.list();
-           //choose category
-           category = categoriesInFolder[categoryIndex];
-           
-//           //debug
-//           System.out.println("3.3");
-//           System.out.println(category);
-//           if (categoryFolder.isDirectory())
-//           System.out.println(categoriesInFolder);
-           
            //create a list of potential files
-           fileFolder = new File("ObjectCategories\\" + category);
-           filesInCategory = fileFolder.list();
+           inputFolder = new File("Input\\");
+           photos = inputFolder.list();
+           numPhotos = photos.length;
            
 //           //debug
 //           System.out.println("3.4");
 //           if (fileFolder.isDirectory())
 //           System.out.println(filesInCategory);
 
-           //select a random file
-           fileIndex = (int) (Math.random()*filesInCategory.length);
-           fileName = filesInCategory[fileIndex];
+           //select the photo and create index or next photo
+           fileName = photos[photoIndex];
+           photoIndex++;
            
 //           //debug
 //           System.out.println("3.5");
 
-           String filePath = "ObjectCategories" + "\\" + category + "\\" + fileName;
-    //       theImage = new APImage(new String("ObjectCategories" + "\\" + category + "\\" + fileName));
+           String filePath = "Input" + fileName;
 
 //           //debug
 //           System.out.println("4");
-//           if (categoryFolder.isDirectory())
-//           System.out.println(filesInCategory);
            
-           //prepare the image for reading and set theImage to it
+           //add the signature to the photo
            theImage = SignatureImagePrep.prepImage(filePath);
            
-           //visual aid for trainer
+           //visual aid
            theImage.draw();
            
         } catch (IOException ex) {
@@ -123,19 +98,7 @@ public class SignatureAdder{
             ex.printStackTrace();
         }
    }
-     
-   public static void readImage(){
-      Scanner reader = new Scanner(System.in);
-      
-      // Read all pixels into input array
-      for (int y = 0; y < theImage.getHeight(); y++) {
-        for (int x = 0; x < theImage.getHeight(); x++){
-            // Obtain info for a pixel and add it to pixel color arrays
-            // at a position corresponding to the y and x values respectively
-            Pixel pixel = theImage.getPixel(x, y);
-        }
-      }
-   }
+    
    
 }
 
--- Signature Adder/src/signature/adder/SignatureImagePrep.java
+++ Signature Adder/src/signature/adder/SignatureImagePrep.java
@@ -11,6 +11,7 @@ package signature.adder;
  */
 
 import images.APImage;
+import java.awt.*;
 import images.Pixel;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -19,26 +20,41 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class SignatureImagePrep {
-    
+    //signature's scale
+            public static int signatureScale;
+            //signature's y scale with respect to the x scale
+            public static int sigYDiff = 8;
     public static APImage prepImage (String path) throws IOException {
         try {
             APImage img = new APImage(path);
+            APImage signature = new APImage();
+            int maxX = img.getWidth();
+            signatureScale = maxX/16;
+            int maxY = img.getHeight();
+            //amount of padding between the signature and sides of photos in pixels
+            int padding = 10;
             
 //            //debug
 //            System.out.println("5");
             
-            //scale image
-            resizeSignature(path, 256);
-
-            //update image
-            img = new APImage(path);
+            //scale signature to 1/16th the photo's width
+            resizeSignature(signatureScale);
             
-            //convert to black and white
+            //"this is where a little studio engineering comes in handy, my hard-rockin' amigo!"
+            //make a BufferedImage object that is virtually the same as the temporary signature, except we can get the RGB values from it
+            BufferedImage tempSig = ImageIO.read(new File("Signature\\Temp Signature.png"));
+
+            //read signature image's pixels to each photo's respective pixels in the photo's lower right-hand corner
+            int x = 0;
+            int y = 0;
             for (Pixel p : img) {
-                int avg = (p.getRed() + p.getGreen() + p.getBlue())/3;
-                p.setRed(avg);
-                p.setGreen(avg);
-                p.setBlue(avg);
+                //if the photo's pixel is within the bounds of a bounding box representing the size of the signature
+                if ((x > maxX - (signatureScale + padding) && (x < maxX - padding)) && (y > maxY - (signatureScale + padding) && (y < maxY - padding))) {
+                    //this is where we use the buffered image data
+                    p.setRed((new Color(tempSig.getRGB(x, y))).getRed());
+                    p.setGreen((new Color(tempSig.getRGB(x, y))).getGreen());
+                    p.setBlue((new Color(tempSig.getRGB(x, y))).getBlue());
+                }
             }
             
 //            //debug
@@ -53,30 +69,30 @@ public class SignatureImagePrep {
     }
     
     //image resizing method
-    public static void resizeSignature(String imagePath, int size) throws IOException {
+    public static void resizeSignature(int size) throws IOException {
         try {
 //            //debug
 //            System.out.println("6");
+            String signaturePath = "Signature\\Signature.png";
             
             // reads input image
-            File inputFile = new File(imagePath);
+            File inputFile = new File(signaturePath);
             BufferedImage inputImage = ImageIO.read(inputFile);
 
             // creates output image
             BufferedImage outputImage = new BufferedImage(size,
-                    size, inputImage.getType());
+                    size/8, inputImage.getType());
 
             // scales the input image to the output image
             Graphics2D g2d = outputImage.createGraphics();
-            g2d.drawImage(inputImage, 0, 0, size, size, null);
+            g2d.drawImage(inputImage, 0, 0, size, size/sigYDiff, null);
             g2d.dispose();
 
             // extracts extension of output file
-            String formatName = imagePath.substring(imagePath
-                    .lastIndexOf(".") + 1);
+            String formatName = signaturePath.substring(signaturePath.lastIndexOf(".") + 1);
 
-            // writes to output file
-            ImageIO.write(outputImage, formatName, new File(imagePath));
+            // writes to output file (temporary, resized signature)
+            ImageIO.write(outputImage, formatName, new File("Signature\\Temp Signature.png"));
 
         } catch (IOException ex) {
             System.out.println("Error resizing the image.");
