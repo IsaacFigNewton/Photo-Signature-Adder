14387e16b8703cdd5067b6b7ed96289a068b0979
IsaacFigNewton <48895941+IsaacFigNewton@users.noreply.github.com>
2020-10-07 14:54:36 -0700

    Almost done
    With some help from one of my teachers, Mr. Crow , I was able to fix some errors.
    It still only processed 3 imiges before running out of memory, but at least it works, as one game dev. so eloquently put it; "It just works."

--- Signature Adder/src/signature/adder/SignatureImagePrep.java
+++ Signature Adder/src/signature/adder/SignatureImagePrep.java
@@ -23,7 +23,7 @@ public class SignatureImagePrep {
     //signature's scale
             public static int signatureScale;
             //signature's y scale with respect to the x scale
-            public static int sigYDiff = 8;
+            public static double sigYDiff = 64/5;
             public static String loadingBar;
             
     public static APImage prepImage (String path) throws IOException {
@@ -33,10 +33,10 @@ public class SignatureImagePrep {
             BufferedImage imgOut = ImageIO.read(new File(path));
             APImage signature = new APImage("Signature\\Signature.jpg");
             int maxX = imgIn.getWidth();
-            signatureScale = maxX/1;
+            signatureScale = maxX/4;
             int maxY = imgIn.getHeight();
             //amount of padding between the signature and sides of photos in pixels
-            int padding = (maxX+maxY)/200;
+            int padding = (maxX+maxY)/400;
             
             //debug
             System.out.print(", 5");
@@ -44,61 +44,53 @@ public class SignatureImagePrep {
             //scale signature to 1/16th the photo's width
             resizeSignature(signatureScale);
             
+            
             //"this is where a little studio engineering comes in handy, my hard-rockin' amigo!"
             //make a BufferedImage object that is virtually the same as the temporary signature, except we can get the RGB values from it
             BufferedImage tempSig = ImageIO.read(new File("Signature\\Temp Signature.jpg"));
+            signatureScale = tempSig.getWidth();
 
             //Start of Loading Bar
             System.out.println("Loading...");
             System.out.printf("[");
             int i = 0;
-            //read signature image's pixels to each photo's respective pixels in the photo's lower right-hand corner
-            int x = 0;
             //the starting x coordinate of the "bounding box"
-            int startX = maxX - signatureScale + padding;
+            int startX = maxX - signatureScale - padding;
+            //read signature image's pixels to each photo's respective pixels in the photo's lower right-hand corner
+            int x = startX;
             //the ending x coordinate of the "bounding box"
             int endX = maxX - padding;
-            int y = 0;
             //the starting x coordinate of the "bounding box"
-            int startY = maxY - signatureScale/sigYDiff + padding;
+            int startY = maxY - (int)(signatureScale/sigYDiff) - padding;
+            
+            int y = startY;
             //the ending y coordinate of the "bounding box"
             int endY = maxY - padding;
-            for (Pixel p : imgIn) {
+            while (y < endY) {
                 //if the photo's pixel is within the bounds of a bounding box representing the size of the signature
                 if ((x > startX && x < endX) && (y > startY && y < endY)) {
-//                    System.out.println("Width: " + maxX);
-//                    System.out.println("x: " + x);
-//                    System.out.println("Height: " + maxY);
-//                    System.out.println("y: " + y);
-                    
+                    //loading bar
                     if (i%50000 == 0)
                         System.out.print("|");
                     
                     //this is where we use the buffered images' data
                     //Get the color of the temporary signature's pixel at the location on the main photo minus the starting coordinates of the "bounding box"
-                    Color tempSigPixelColor = new Color(tempSig.getRGB(x - startX, y - startY));
                     //Then set the respective pixel on the main photo to that color
-                    imgOut.setRGB(tempSigPixelColor.getRed(), tempSigPixelColor.getGreen(), tempSigPixelColor.getBlue());
+                    imgOut.setRGB(x, y, tempSig.getRGB(x - startX, y - startY));
                 }
                 
-                if (x >= maxX)
+                //move 
+                if (x >= maxX){
                     x = 0;
-                else
-                    x++;
-                
-                if (y >= maxY)
-                    y = 0;
-                else
                     y++;
-                
+                } else {
+                    x++;
+                }
                 i++;
             }
             //end of Loading Bar
             System.out.println("]");
             
-            //Logical error is in section above
-            System.out.println("Logical malfunction SignatureImagePrep.java; will only draw pixels of signature as diagonal, 45-degree black line in upper-right hand corner");
-            System.out.println("See line 95 for slightly more information.");
             //debug
             System.out.print("7");
             
@@ -136,7 +128,7 @@ public class SignatureImagePrep {
 
             // scales the input image to the output image
             Graphics2D g2d = outputImage.createGraphics();
-            g2d.drawImage(inputImage, 0, 0, size, size/sigYDiff, null);
+            g2d.drawImage(inputImage, 0, 0, size, (int)(size/sigYDiff), null);
             g2d.dispose();
 
             // extracts extension of output file
@@ -150,4 +142,5 @@ public class SignatureImagePrep {
             ex.printStackTrace();
         }
     }
+    
 }
